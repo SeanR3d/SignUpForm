63906a45baa23721c8efe1103aa51cc5bf14ab54
Sean <Seanr1194@gmail.com>
2019-09-18 10:55:29 -0400

    added some on click events

--- app/src/main/java/edu/tuh42070/signupform/FormActivity.java
+++ app/src/main/java/edu/tuh42070/signupform/FormActivity.java
@@ -2,7 +2,10 @@ package edu.tuh42070.signupform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +13,44 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.SignUpButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameView = findViewById(R.id.NameEditText);
+                String name = nameView.getText().toString();
+                if (checkEmptyField(name)){
+                    nameView.setHint("Required Field!");
+                    nameView.setHintTextColor(Color.RED);
+                };
+
+                EditText emailView = findViewById(R.id.EmailEditText);
+                String email = emailView.getText().toString();
+
+                EditText passwordView = findViewById(R.id.PasswordEditText);
+                String password = passwordView.getText().toString();
+
+                EditText confirmPasswordView = findViewById(R.id.ConfirmPasswordEditText);
+                String confirmPassword = confirmPasswordView.getText().toString();
+
+
+                // check empty fields
+                // check passwords
+                // display welcome message
+
+            }
+        });
     }
+
+    private Boolean checkEmptyField(String fieldText){
+        if (fieldText.length() == 0 || fieldText == null)
+            return false;
+
+        return true;
+    }
+
+    private Boolean confirmPasswords(String password, String confirmPassword){
+        return false;
+    }
+
 }
