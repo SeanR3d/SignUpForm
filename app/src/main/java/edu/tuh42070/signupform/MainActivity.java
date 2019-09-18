af60158936553392a546f6155edac4c1423cc745
Sean <Seanr1194@gmail.com>
2019-09-18 09:17:06 -0400

    Generated project files

---
+++ app/src/main/java/edu/tuh42070/signupform/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.tuh42070.signupform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
